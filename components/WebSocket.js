55c82d77ba6de52ceb49b8d5b9a72a2ba39db2c0
subrahmanya <subrahmanyavaidya7@gmail.com>
2025-01-31 20:40:24 +0530

    made both server run on same port

--- components/WebSocket.js
+++ components/WebSocket.js
@@ -1,23 +1,43 @@
 //Web socket file
-import { createServer } from "http";
-import { WebSocketServer } from "ws";
-import handleMessage from "./OpenAi.js"; // Import Gemini function
 
-const server = createServer();
-const wss = new WebSocketServer({ server });
 
-wss.on("connection", (ws) => {
+
+// const server = createServer();
+// const wss = new WebSocketServer({ server });
+
+// wss.on("connection", (ws) => {
   
-  ws.on("message", async (message) => {
-    const userQuery = message.toString();
+//   ws.on("message", async (message) => {
+//     const userQuery = message.toString();
 
-    const response = await handleMessage(userQuery); // Call Gemini API
-    ws.send(response); // Send AI response back to client
-  });
+//     const response = await handleMessage(userQuery); // Call Gemini API
+//     ws.send(response); // Send AI response back to client
+//   });
 
-  ws.on("close", () => {});
-});
+//   ws.on("close", () => {});
+// });
 
-server.listen(8080, () => {
-  console.log("WebSocket server is running on ws://localhost:8080");
-});
+// server.listen(8080, () => {
+//   console.log("WebSocket server is running on ws://localhost:8080");
+// });
+import { WebSocketServer } from "ws";
+import handleMessage from "./OpenAi.js"; // Import Gemini 
+const setUpWebSocket = ( server ) =>{
+  const wss = new WebSocketServer({ server });
+  console.log("Web socket server Started!");
+  wss.on("connection", (ws)=>{
+    console.log("new client connected!")
+    ws.on("message", async (message)=>{
+      const userQuery = message.toString();
+      const response = await handleMessage(userQuery);
+      ws.send(response);
+    })
+
+    ws.on("close", ()=>{
+      console.log("Client disconnected!");
+    });
+  })
+}
+
+
+export default setUpWebSocket;
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 //entry point for the server
-
 import express from "express";
+import { createServer } from "http";
 import dotenv from "dotenv";
-import "./components/WebSocket.js"
+import setUpWebSocket from "./components/WebSocket.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const wsPort = process.env.WS_PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Express server running on http://localhost:${port}`);
+const server = createServer(app);
+setUpWebSocket(server);
+
+server.listen(port, () => {
+  console.log(`Express and webSocket running on port: ${port}`);
 });
